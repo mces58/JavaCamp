b56b3dfbbf40b8e5d1c003ab325d13ec08b55c43
can <72516854+mces58@users.noreply.github.com>
2022-11-05 17:25:38 +0300

    Delete hafta5/Kodlama.io.Devs directory

--- hafta5/Kodlama.io.Devs/src/main/java/kodlama/io/devs/repository/LanguageRepository.java
+++
@@ -1,8 +0,0 @@
-package kodlama.io.devs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlama.io.devs.entities.Language;
-
-public interface LanguageRepository extends JpaRepository<Language, Integer> {
-}
--- hafta5/Kodlama.io.Devs/src/main/java/kodlama/io/devs/repository/LanguageTechnologyRepository.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.devs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlama.io.devs.entities.LanguageTechnology;
-
-public interface LanguageTechnologyRepository extends JpaRepository<LanguageTechnology, Integer> {
-
-}
